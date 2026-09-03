b57b78f78cd130b1d0424e99421c5da843b60c4b
Pranav Desai <contact.pranavdesai@gmail.com>
2026-09-03 19:22:41 +0530

    feat: write main

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+var db *sql.DB
 
 func main() {
-	fmt.Println("S.N.E")
+	var err error
+	db, err = sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("Failed to connect to db %e", err)
+	}
+	defer db.Close()
+
+	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
+	}))
+
+	r.Get("/habitations", getHabitations)
+	r.Get("/risk-zones", getRiskZones)
+	r.Get("/sites", getSites)
 }
